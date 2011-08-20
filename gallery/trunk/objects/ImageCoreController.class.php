1cc253a539078b2ce29231c51c80883bc975f034
geusebio <geusebio@9200b8d4-8e4c-4fa3-b4c8-4925269e3ab4>
2011-08-20 09:34:25 +0000

    Caching fixes\!
    git-svn-id: http://svn.turbocrms.com/magicplugins@41 9200b8d4-8e4c-4fa3-b4c8-4925269e3ab4

--- gallery/trunk/objects/ImageCoreController.class.php
+++ gallery/trunk/objects/ImageCoreController.class.php
@@ -24,15 +24,49 @@
 
 class ImageCoreController extends ImageBaseController {
 	public function ThumbAction(){
+		
+		//set time to expire in seconds
+		$expires = 86400;
+		
+		//get the contents of the buffer into a variable
 		Application::$nocache = true;
-	    	$bits = explode("-",str_replace(".jpg", "", $_GET['parameter']),2);
-	    	$dimentions = explode("x",$bits[0],2);
-	    	$image_id = base_convert($bits[1], 36, 10);
-		//print_r($_GET);
+    	$bits = explode("-",str_replace(".jpg", "", $_GET['parameter']),2);
+    	$dimentions = explode("x",$bits[0],2);
+    	$image_id = base_convert($bits[1], 36, 10);
 		$oImage = ImageSearcher::Factory()->search_by_id($image_id)->execute_one();
-		$image_data = $oImage->scale_to_fit($dimentions[0],$dimentions[1]);
+		$cache_time = $oImage->get_uploaded();
+		$body = $oImage->scale_to_fit($dimentions[0],$dimentions[1]);
+		// Etag and last modified checking and Generation
+
+		$send_body = true;
+		$etag = '"' . md5($body) . '"';
+		header ("ETag: " . $etag );
+		header("last-modified: " . gmdate("D, d M Y H:i:s",$cache_time) . " GMT");
+		$inm = split(',', getenv("HTTP_IF_NONE_MATCH"));
+		foreach ($inm as $i){
+			if (trim($i) == $etag || trim($i) == $cache_time){
+				header ("HTTP/1.0 304 Not Modified");
+				$send_body = false;
+			}
+		}
+	
+		// last modified test
+		if(getenv("HTTP_IF_MODIFIED_SINCE") == gmdate("D, d M Y H:i:s",$cache_time). " GMT"){
+			header ("HTTP/1.0 304 Not Modified");
+			$send_body = false;
+		}
+		
+		// more headers
+		header("Expires: " . gmdate("D, d M Y H:i:s",$cache_time + $expires) . " GMT");
+		header("Cache-Control: max-age=$expires, must-revalidate");
+		header('Content-Length: ' . strlen($body));
 		header('Content-Type: image/jpeg');
-		echo $image_data;
+
+
+//if we're not cacheing
+
+if($send_body) print $body;
+		
 		exit;
 	}
 }
