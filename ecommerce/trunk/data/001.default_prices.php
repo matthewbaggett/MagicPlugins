a64fe952ad6d8a1b0be7e4cc0773fdbfa2bcdbb5
geusebio <geusebio@9200b8d4-8e4c-4fa3-b4c8-4925269e3ab4>
2011-09-13 15:48:42 +0000

    TEst price generator
    git-svn-id: http://svn.turbocrms.com/magicplugins@93 9200b8d4-8e4c-4fa3-b4c8-4925269e3ab4

---
+++ ecommerce/trunk/data/001.default_prices.php
@@ -0,0 +1,28 @@
+<?php
+if(!PriceTypeSearcher::Factory()->search_by_handle("DEFAULT")->count() > 0){
+	PriceType::Factory()
+		->set_name("Default")
+		->set_description("Default price band")
+		->set_handle("DEFAULT")
+		->save();
+}
+
+if(!PriceTypeSearcher::Factory()->search_by_handle("TEST")->count() > 0){
+	PriceType::Factory()
+		->set_name("Test pricing")
+		->set_description("The test price band")
+		->set_handle("TEST")
+		->save();
+}
+
+$oTestPriceType = PriceTypeSearcher::Factory()->search_by_handle("TEST")->execute_one();
+foreach(PartSearcher::Factory()->execute() as $oPart){
+	$oPart = Part::Cast($oPart);
+	if(!$oPart->has_child_prices()){
+		Price::Factory()
+			->set_part($oPart->get_id())
+			->set_pricetype($oTestPriceType->get_id())
+			->set_amount(rand(5,50))
+			->save();
+	}
+}
