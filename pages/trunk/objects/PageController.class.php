ea5ca9f8a918434a2de7dfb5971a8ec52a9a2bc6
geusebio <geusebio@9200b8d4-8e4c-4fa3-b4c8-4925269e3ab4>
2011-09-15 10:50:31 +0000

    git-svn-id: http://svn.turbocrms.com/magicplugins@99 9200b8d4-8e4c-4fa3-b4c8-4925269e3ab4

--- pages/trunk/objects/PageController.class.php
+++ pages/trunk/objects/PageController.class.php
@@ -7,15 +7,20 @@ class PageController extends PageBaseController {
 	
 	public function ViewAction(){
         $this->application->page_setup();
-        $this->page = PageSearcher::Factory()->search_by_path($_REQUEST['parameter'])->execute_one();
+        $this->page = PageSearcher::Factory()
+        		->search_by_path($_REQUEST['parameter'])
+        		->search_by_published("yes")
+        		->search_by_deleted("no")
+        		->sort("version",'DESC')
+        		->execute_one();
         if(!$this->page){
         	throw new exception("No page exists for this path :(");
         }
-        $versions = $this->page->get_child_page_versions("id");
-        $this->page_version = array_pop($versions);
-        $navigation = PageSearcher::Factory()->search_by_parent_id(0)->execute();
-        $this->application->page->cmspage = $this->page_version;
-        $this->application->page->title = $this->application->page->cmspage->get_title();
+        $navigation = PageSearcher::Factory()
+        		->search_by_parent_id(0)
+        		->execute();
+        $this->application->page->content = $this->page->get_content();
+        $this->application->page->title = $this->page->get_title();
         $this->application->page->navigation = $navigation;
         $this->application->page->navigation_structure = $this->get_navigation_structure($navigation);
        	
@@ -36,9 +41,9 @@ class PageController extends PageBaseController {
     		}
     		$tree[] = array(
     					"path" => $node->get_path(),
-    					"title" => $node->get_child_page_version()->get_title(),
+    					"title" => $node->get_title(),
     					"children" => $children,
-    					"selected" => $this->page_version->get_id()==$node->get_child_page_version()->get_id()?'yes':'no',
+    					"selected" => $this->get_id() == $node->get_id()?'yes':'no',
     					"selected_path" => $this->find_selected_in_path($node)?'yes':'no'
     			  );
     	}
@@ -58,7 +63,7 @@ class PageController extends PageBaseController {
     	 * if the page that we've actually loaded matches this node,
     	 * its a match (since its the page currently displayed, ergo, MUST be in the path
     	 */ 
-    	if($this->page_version->get_id()==$node->get_child_page_version()->get_id()){
+    	if($this->get_id()==$node->get_id()){
     		return true;
     	}elseif(count($children) > 0){
     		foreach((array) $children as $child){
